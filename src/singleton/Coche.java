8395e7c923985f813009f03343920eebd8beb2aa
scuenca77 <scuenca77@gmail.com>
2014-11-08 18:22:49 +0100

    Uploading class: Coche.java

---
+++ src/singleton/Coche.java
@@ -0,0 +1,35 @@
+package singleton;
+
+public class Coche {
+	
+	private String marca;
+	private String modelo;
+	private int cilindrada;
+	private int consumo;
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getCilindrada() {
+		return cilindrada;
+	}
+	public void setCilindrada(int cilindrada) {
+		this.cilindrada = cilindrada;
+	}
+	public int getConsumo() {
+		return consumo;
+	}
+	public void setConsumo(int consumo) {
+		this.consumo = consumo;
+	}
+
+}
